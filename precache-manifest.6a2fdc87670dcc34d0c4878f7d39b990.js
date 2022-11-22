b4d1002bb0d3d211ba998604f39ec1a7049be507
uttamdawadi <uttam.dawadi1107@gmail.com>
2022-11-22 13:40:26 +1100

    Updates

---
+++ precache-manifest.6a2fdc87670dcc34d0c4878f7d39b990.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd0ac665cb47f72a49d9f5ea2f6b8aa0",
+    "url": "/udawadi/index.html"
+  },
+  {
+    "revision": "e0c413e5d3273fe81247",
+    "url": "/udawadi/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "8b9aa16a9356081b243c",
+    "url": "/udawadi/static/css/4.d7ceb645.chunk.css"
+  },
+  {
+    "revision": "cbe6d06757fcbc6752fa",
+    "url": "/udawadi/static/css/main.87aa1b4e.chunk.css"
+  },
+  {
+    "revision": "8d864a1fc592cde551e5",
+    "url": "/udawadi/static/js/2.0ea6c907.chunk.js"
+  },
+  {
+    "revision": "b045256696e9edb485ef902b6be54c14",
+    "url": "/udawadi/static/js/2.0ea6c907.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0c413e5d3273fe81247",
+    "url": "/udawadi/static/js/3.72feead1.chunk.js"
+  },
+  {
+    "revision": "8b9aa16a9356081b243c",
+    "url": "/udawadi/static/js/4.8575ba78.chunk.js"
+  },
+  {
+    "revision": "cbe6d06757fcbc6752fa",
+    "url": "/udawadi/static/js/main.b8845148.chunk.js"
+  },
+  {
+    "revision": "9429f498361ef438ed6c",
+    "url": "/udawadi/static/js/runtime-main.378577a8.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/udawadi/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/udawadi/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/udawadi/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/udawadi/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/udawadi/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/udawadi/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/udawadi/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "bb7f43255e744458b470b18d0a660ff3",
+    "url": "/udawadi/static/media/federation_Logo.bb7f4325.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/udawadi/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/udawadi/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/udawadi/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "bfaeacd5e66154152b7efcf0ebd0b856",
+    "url": "/udawadi/static/media/profilePic.bfaeacd5.gif"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/udawadi/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/udawadi/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/udawadi/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/udawadi/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "88be0f6264c043efc8cfb0eaf8b5e40f",
+    "url": "/udawadi/static/media/softwarica logo.88be0f62.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/udawadi/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
